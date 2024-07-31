4866242f8bef29234f694343df89e23d103ef028
squishedfox <christopher.schoeder@protonmail.com>
2024-07-30 21:57:23 -0500

    create basic schema for entities

--- main.go
+++ main.go
@@ -1,18 +1,56 @@
 package main
 
 import (
-	"github.com/freightcms/api-template/api"
-	"github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/freightcms/webservice-template/schemas"
+	"github.com/freightcms/webservice-template/web"
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
 )
 
 func main() {
-	server := gin.Default()
-
-	v1Routes := server.Group("/v1")
-
-	api.Register(v1Routes)
-
-	if err := server.Run(":3001"); err != nil {
+	rootQuery := graphql.NewObject(graphql.ObjectConfig{
+		Name: "RootQuery",
+		Fields: graphql.Fields{
+			"entityList": &graphql.Field{
+				Description: "Get All entities",
+				Type:        web.PaginatedEntitiesObject,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					return &struct {
+						ETag        string                 `json:"etag"`
+						Entities    []schemas.EntitySchema `json:"entities"`
+						Count       int                    `json:"count"`
+						PageSize    int                    `json:"pageSize"`
+						Page        int                    `json:"page"`
+						NextUrl     string                 `json:"nextURL"`
+						PreviousUrl string                 `json:"previousURL"`
+					}{
+						ETag:        "",
+						Entities:    []schemas.EntitySchema{},
+						Count:       0,
+						PageSize:    10,
+						Page:        0,
+						NextUrl:     "",
+						PreviousUrl: "",
+					}, nil
+				},
+			},
+		},
+	})
+	schema, err := graphql.NewSchema(graphql.SchemaConfig{
+		Query: rootQuery,
+	})
+	if err != nil {
 		panic(err)
 	}
+
+	h := handler.New(&handler.Config{
+		Schema:   &schema,
+		Pretty:   true,
+		GraphiQL: true,
+	})
+
+	http.Handle("/graphql", h)
+	http.ListenAndServe(":8080", nil)
 }
--- web/web.go
+++ web/web.go
@@ -3,11 +3,61 @@ package web
 import "github.com/graphql-go/graphql"
 
 var (
-	entitiesQuery graphql.ObjectConfig
-)
-
-func init() {
-	entitiesQuery = graphql.NewObjectConfig(
-	graphql.NewObject({
+	// EntityObject is the GraphQL schema used for the Generic Entity of the
+	// API to fetch. This can be replaced with your real Entity Implemntation.
+	EntityObject *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
+		Name: "Entity",
+		Fields: graphql.Fields{
+			"prop1": &graphql.Field{
+				Type: graphql.String,
+				Name: "Prop1",
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					return "hello", nil
+				},
+			},
+			"prop2": &graphql.Field{
+				Type: graphql.Int,
+				Name: "Prop2",
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					return "world", nil
+				},
+			},
+		},
 	})
-}
+	// PaginatedEntitiesObject reveals the standardized response for a paginated list of entities
+	// fetched from another web service, or data system. This can be re-used across one or many
+	// different types of Paginated Entity types.
+	PaginatedEntitiesObject *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
+		Name: "Entities",
+		Fields: graphql.Fields{
+			"etag": &graphql.Field{
+				Type: graphql.String,
+				Name: "Etag",
+			},
+			"entities": &graphql.Field{
+				Type: graphql.NewList(EntityObject),
+				Name: "Entities",
+			},
+			"page": &graphql.Field{
+				Type: graphql.Int,
+				Name: "Page",
+			},
+			"count": &graphql.Field{
+				Type: graphql.Int,
+				Name: "Count",
+			},
+			"pageSize": &graphql.Field{
+				Type: graphql.Int,
+				Name: "pageSize",
+			},
+			"nextUrl": &graphql.Field{
+				Type: graphql.String,
+				Name: "NextURL",
+			},
+			"previousUrl": &graphql.Field{
+				Type: graphql.String,
+				Name: "PreviousURL",
+			},
+		},
+	})
+)
