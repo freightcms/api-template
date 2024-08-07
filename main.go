d83d3f798c59016c211286082f84641d202d541d
squishedfox <christopher.schoeder@protonmail.com>
2024-08-06 21:24:04 -0500

    wip

--- main.go
+++ main.go
@@ -13,18 +13,12 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func ServeHTTP(h *handler.Handler) http.HandlerFunc {
-	// this is a hack around the internal go-graphql ServeHTTP because the API
-	// does not appropriately expose the request context or a good way to add
-	// middleware into requests.
-	return func(w http.ResponseWriter, r *http.Request) {
-		session, err := client.StartSession(&options.SessionOptions{})
-		if err != nil {
-			panic(err)
-		}
-		ctx := context.WithValue(r.Context(), "dbSession", session)
-		h.ContextHandler(ctx, w, r)
-	}
+func GetMongoClient() (*mongo.Client, error) {
+	uri := os.Getenv("MONGODB_URI")
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	return mongo.Connect(context.Background(), opts)
 }
 
 func main() {
@@ -32,11 +26,7 @@ func main() {
 		panic(err)
 	}
 
-	uri := os.Getenv("MONGODB_URI")
-	// Use the SetServerAPIOptions() method to set the Stable API version to 1
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
-	client, err := mongo.Connect(context.Background(), opts)
+	client, err := GetMongoClient()
 	if err != nil {
 		panic(err)
 	}
@@ -64,12 +54,8 @@ func main() {
 		Pretty:   true,
 		GraphiQL: true,
 	})
+	srvr := &http.ServeMux{}
+	srvr.Handle("/graphql", h)
 
-	middlewareHandler := struct {
-		ServeHTTP http.HandlerFunc
-	}{
-		ServeHTTP: ServeHTTP(h),
-	}
-	http.Handle("/graphql", middlewareHandler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":3000", srvr)
 }
